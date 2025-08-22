ae2c82d15597a837624bdc3c67d17f4e5632116e
Tshering-Wangchuk <tsheringwangchuky@gmail.com>
2025-08-22 10:10:43 +0000

    Events

--- education/event_management/doctype/event_attendance/event_attendance.py
+++ education/event_management/doctype/event_attendance/event_attendance.py
@@ -3,6 +3,7 @@
 
 import frappe
 from frappe.model.document import Document
+from frappe import _
 
 
 class EventAttendance(Document):
@@ -11,22 +12,21 @@ class EventAttendance(Document):
 @frappe.whitelist()
 def create_attendance(dt, dn):
     event = frappe.get_doc(dt, dn)
+    if event.registration_type != "Class":
+        frappe.throw(_("Attendance can only be created for class events."))
 
-    if event.registration_type != "Faculty":
-        frappe.throw(_("Attendance can only be created for Faculty events."))
-
+    if not event.get("student_register") or len(event.student_register) == 0:
+        frappe.throw(_("Cannot create attendance - Student Register is empty. Please add students first."))    
     attendance = frappe.new_doc("Event Attendance")
     attendance.event = event.name
     attendance.attendance_date = event.start_date.date() if event.start_date else frappe.utils.today()
-
-    for row in event.event_register:
-        if not row.faculty_email:
+    for row in event.student_register:
+        if not row.student_email:
             continue
         attendance.append("attendance_list", {
-            "student_name": row.faculty_name
+            "student_name": row.student_name,
         })
     attendance.insert(ignore_permissions=True)
-
     return {
         "status": "success",
         "attendance_name": attendance.name
--- education/event_management/doctype/events/events.js
+++ education/event_management/doctype/events/events.js
@@ -1,7 +1,5 @@
 frappe.ui.form.on("Events", {
     refresh(frm) {
-
-
         frm.call("has_attendance").then((r) => {
             if (!r.message.has_attendance) {
                 if (frm.doc.docstatus === 1) {
@@ -17,7 +15,7 @@ frappe.ui.form.on("Events", {
                 if (frm.doc.name) {
                     frm.add_custom_button(__('View Attendance'), function () {
                         frappe.set_route('List', 'Event Attendance', {
-                            concept_note: frm.doc.name
+                            student: frm.doc.name
                         });
                     });
                 }
@@ -26,7 +24,6 @@ frappe.ui.form.on("Events", {
         });
 
     },
-
     create_attendace: function (frm) {
         let method = "education.event_management.doctype.event_attendance.event_attendance.create_attendance";
         return frappe.call({
@@ -46,14 +43,12 @@ frappe.ui.form.on("Events", {
         });
     },
 
-
     get_all_faculty(frm) {
         frappe.call({
             method: "frappe.client.get_list",
             args: {
                 doctype: "User",
-                limit_page_length: 1000,
-                fields: ["name", "email", "full_name"]
+                fields: ["name", "email", "full_name"],
             },
             callback: function (r) {
                 if (r.message && r.message.length) {
@@ -64,21 +59,76 @@ frappe.ui.form.on("Events", {
         });
     },
 
+    get_all_student(frm) {
+        frappe.call({
+            method: "frappe.client.get_list",
+            args: {
+                doctype: "Student",
+                fields: ["name", "student_email_id", "first_name"],
+            },
+            callback: function (r) {
+                if (r.message && r.message.length) {
+                    frm.doc._all_student = r.message;
+                    reload_student_rows(frm);
+                }
+            }
+        });
+    },
+
+    // get_all_student(frm) {
+    //     frm.clear_table("student_register");
+    //     frappe.call({
+    //         method: "frappe.client.get_list",
+    //         args: {
+    //             doctype: "Student",
+    //             fields: ["name", "student_email_id", "first_name"],
+    //         },
+    //         callback: function (r) {
+    //             if (r.message && r.message.length) {
+    //                 r.message.forEach(std => {
+    //                     let row = frm.add_child("student_register");
+    //                     row.student = std.name;
+    //                     row.student_email = std.student_email_id;
+    //                     row.student_name = std.first_name;
+    //                 });
+    //                 frm.refresh_field("student_register");
+    //             }
+    //         }
+    //     });
+    // }
+
+
 
 });
 
 function reload_faculty_rows(frm) {
     frm.clear_table("event_register");
-
     let faculty_to_show = frm.doc._all_faculty.slice(0);
     faculty_to_show.forEach(emp => {
         let row = frm.add_child("event_register");
-        row.faculty_name = emp.name;
+        row.faculty = emp.name;
         row.faculty_email = emp.email;
-        row.faculty = emp.full_name;
+        row.faculty_name = emp.full_name;
     });
 
     frm.refresh_field("event_register");
 }
+
+
+function reload_student_rows(frm) {
+    frm.clear_table("student_register");
+    let faculty_to_show = frm.doc._all_student.slice(0);
+    faculty_to_show.forEach(std => {
+        let row = frm.add_child("student_register");
+        row.student = std.name;
+        row.student_email = std.student_email_id;
+        row.student_name = std.first_name;
+    });
+    frm.refresh_field("student_register");
+}
+
+
+
+
 
 

e1cee6fe2b6b77a89c6eb64a91722578b3c50864
kinleydorji <kd77144@gmail.com>
2026-08-17 10:36:08 +0600

    student attendance changes

--- education/education/api.py
+++ education/education/api.py
@@ -90,7 +90,6 @@ def check_attendance_records_exist(course_schedule=None, student_group=None, dat
 def mark_attendance(
 	students_present, students_absent, from_time, to_time, day, timetable_schedule_entry, academic_year, academic_term, programme=None, student_group=None, date=None, course = None
 ):
-	frappe.throw("Programme: "+programme+", Academic Term: "+academic_term+", Academic Year: "+academic_year)
 	"""Creates Multiple Attendance Records.
 
 	:param students_present: Students Present JSON.
@@ -117,12 +116,12 @@ def mark_attendance(
 
 	for d in present:
 		make_attendance_records(
-			d["student"], d["student_name"], "Present", from_time, to_time, timetable_schedule_entry, day, student_group, programme,  academic_year, academic_term, date=date, course=course
+			d["student"], d["student_name"], "Present", from_time, to_time, timetable_schedule_entry, day, student_group, academic_year, academic_term, programme = programme, date=date, course=course
 		)
 
 	for d in absent:
 		make_attendance_records(
-			d["student"], d["student_name"], "Absent", from_time, to_time, timetable_schedule_entry, day, student_group, programme, academic_year, academic_term, date=date, course=course
+			d["student"], d["student_name"], "Absent", from_time, to_time, timetable_schedule_entry, day, student_group, programme, academic_year, academic_term, programme = programme, date=date, course=course
 		)
 
 	frappe.db.commit()
@@ -130,7 +129,7 @@ def mark_attendance(
 
 @frappe.whitelist()
 def make_attendance_records(
-	student, student_name, status, from_time, to_time, timetable_schedule_entry, day, programme, academic_year, academic_term, student_group=None, date=None, course=None
+	student, student_name, status, from_time, to_time, timetable_schedule_entry, day, academic_year, academic_term, programme=None, student_group=None, date=None, course=None
 ):
 	"""Creates/Update Attendance Record.
 
